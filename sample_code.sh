15263b085e26a240b2f6f2c0585dd4a75096ef0b
Vindaru <79673706+Vindaru@users.noreply.github.com>
2021-06-22 10:34:01 +0530

    Second Commit

--- sample_code.sh
+++ sample_code.sh
@@ -5,6 +5,5 @@ echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
-echo "This is to test Git hub integration with Jenkins"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
